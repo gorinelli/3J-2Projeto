fa39b253be1b3ff90d60c76f84e18c924693e9c3
gorinelli <106194389+gorinelli@users.noreply.github.com>
2024-10-03 08:07:55 -0300

    03-10

---
+++ graficos/informacoesGlobais.js
@@ -0,0 +1,19 @@
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+
+async function visualizarDadosGlobais(){
+    const res = await fetch(url);
+    const secao = document.querySelector('.graficos-container');
+    const dados = await res.json();
+    const totalPessoasMundo = dados.total_pessoas_mundo
+    const totalPessoasConectadas = dados.total_pessoas_conectadas
+    const tempoMedio = dados.tempo_medio
+    cconsole.log(dados);
+    const paragrafo = document.createElement('p');
+    paragrafo.classList.add('graficos-texto');
+    paragrafo.innerHTML = 'você sabbia que o mundo tem ${totalPessoasMundo} de habitantes e destes ${totalPessoasConectadas} estão conectadas a alguma rede social e ficam em méedia ${tempoMedio} conectadas.'
+    secao.appendChild(paragrafo)
+}
+
+
+
+visualizarDadosGlobais();
