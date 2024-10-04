ca08a58ec69acb65378f5d92aa0dac8a7024f3bc
gorinelli <106194389+gorinelli@users.noreply.github.com>
2024-10-04 09:44:20 -0300

    04-10

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -2,15 +2,15 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 
 async function visualizarDadosGlobais(){
     const res = await fetch(url);
-    const secao = document.querySelector('.graficos-container');
+    const secao = document.querySelector('.graficos-container')
     const dados = await res.json();
     const totalPessoasMundo = dados.total_pessoas_mundo
     const totalPessoasConectadas = dados.total_pessoas_conectadas
     const tempoMedio = dados.tempo_medio
-    cconsole.log(dados);
+    console.log(dados);
     const paragrafo = document.createElement('p');
-    paragrafo.classList.add('graficos-texto');
-    paragrafo.innerHTML = 'você sabbia que o mundo tem ${totalPessoasMundo} de habitantes e destes ${totalPessoasConectadas} estão conectadas a alguma rede social e ficam em méedia ${tempoMedio} conectadas.'
+    paragrafo.classList.add('graficos-texto')
+    paragrafo.innerHTML = `Você sabia que o mundo tem  <span> ${totalPessoasMundo} </span> de habitantes e destes <span> ${totalPessoasConectadas} </span> estão conectadas a alguma rede social e ficam em média <span> ${tempoMedio} </span> conectadas.`
     secao.appendChild(paragrafo)
 }
 
