3dfcc5aaf5308d07c5648e2c008a4946ce4b3dfe
gorinelli <106194389+gorinelli@users.noreply.github.com>
2024-11-07 07:47:35 -0300

    Update quantidadeDeUsuarios.js

--- graficos/quantidadeDeUsuarios.js
+++ graficos/quantidadeDeUsuarios.js
@@ -1,40 +1,57 @@
-import { getCSS } from "./comum.js";
-import { tickfont } from "./comum.js";
+import { getCSS, tickfont } from "./comum.js";
 
-async function quantidadeDeUsuarios(){
-    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
-    const resultado = await fetch(url);
-    const dados = await resultado.json();
-    const nomeDasRedes =Object.keys(dados);
-    const quantidadeDeUsuarios = Object.values(dados);
+async function quantidadeDeUsuarios() {
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json&#39;
+const resultado = await fetch(url);
+const dados = await resultado.json();
+const nomeDasRedes = Object.keys(dados);
+const quantidadeDeUsuarios = Object.values(dados);
 
-    const infos = [
-        {
-        x: nomeDasRedes,
-        y: quantidadeDeUsuarios,
-        type: 'bar'
-    }
+const infos = [
+{
+x: nomeDasRedes,
+y: quantidadeDeUsuarios,
+type: 'bar',
+marker: {
+color: getCSS('--cor-de-primaria')
+}
+}
 ]
 
-    const layout = {
-        plot_bgcolor: getCSS ('--cor-de-fundo'),
-        paper_bgcolor: getCSS ('--cor-de-fundo'),
-        title: {
-            text: 'Redes sociais com mais usuário do mundo',
-            x: 0,
-            font:{
-                color: getCSS('--cor-primaria-'),
-                family: getCSS('--fonte'),
-                size: 30
-            }
-        }
-    }
-
-      const grafico = document.createElement('div');
-      grafico.className = 'grafico';
-      document.getElementById('graficos-container').appendChild(grafico);
-      Plotly.newPlot(grafico,infos,layout)
-
+const layout = {
+plot_bgcolor: getCSS('--cor-de-fundo'),
+paper_bgcolor: getCSS('--cor-de-fundo'),
+title: {
+text: 'Redes sociais com mais usuários no mundo',
+x: 0,
+font: {
+color: getCSS('--cor-de-primaria'),
+family: getCSS('--fonte'),
+size: 30
 }
-
+},
+xaxis: {
+tickfont: tickfont,
+title: {
+text: 'Nome das redes sociais',
+font: {
+color: getCSS('--cor-secundaria')
+}
+}
+},
+yaxis: {
+tickfont: tickfont,
+title: {
+text: 'Milhões de usuários ativos',
+font: {
+color: getCSS('--cor-secundaria')
+}
+}
+}
+}
+const grafico = document.createElement('div');
+grafico.className = 'grafico';
+document.getElementById('graficos-container').appendChild(grafico);
+Plotly.newPlot(grafico, infos, layout)
+}
 quantidadeDeUsuarios()
